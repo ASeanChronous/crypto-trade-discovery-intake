c2199d9ac76c3c304be72597c62ce03cf5bbd959
TE DevHub <sean@tradersedgegroup.com>
2025-09-19 14:01:43 -0400

    Update layout.js
    ver 1.3

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,9 @@
+import './globals.css'
+
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body style={{ margin: 0, fontFamily: 'Arial' }}>
+      <body>
         {children}
       </body>
     </html>
