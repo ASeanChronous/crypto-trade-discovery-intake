00b1846e602749c0dcba731848992bb848e3190c
TE DevHub <sean@tradersedgegroup.com>
2025-09-22 13:40:24 -0400

    Update page.js
    updated page components

--- app/page.js
+++ app/page.js
@@ -1,6 +1,6 @@
 'use client'
-import CryptoTradeDiscoveryIntake from '../components/CryptoTradeDiscoveryIntake'
+import CompleteCryptoPlatform from '../components/CryptoTradeDiscoveryIntake'
 
 export default function Home() {
-  return <CryptoTradeDiscoveryIntake />
+  return <CompleteCryptoPlatform />
 }
